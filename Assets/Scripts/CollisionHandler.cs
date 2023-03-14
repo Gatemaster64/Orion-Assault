a8c865326523500c44bbb023bc8793cdabaee53b
Gatemaster64 <daniel.roestenburg01@outlook.com>
2023-03-14 22:51:42 +0100

    Edited the colllision script to load the current scene (not done)

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelLoadDelay = 1f;
     private void OnCollisionEnter (Collision other)
     // When colliding with something get information of the other object collided with 
     {
-        Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
+        StartCrashSequence();
+        //Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
     }
+    
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name}**Triggered by **{other.gameObject.name}");  
+        StartCrashSequence();
+        //Debug.Log($"{this.name}**Triggered by **{other.gameObject.name}");  
     }
+
+
+    void StartCrashSequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = (SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+
 
 }
