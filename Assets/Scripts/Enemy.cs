035c490f9f7f0a99b8786d5048b8e3b22c70e0be
Gatemaster64 <daniel.roestenburg01@outlook.com>
2023-03-21 18:51:56 +0100

    Edited the Enemy script to give the enemy hitpoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,8 +7,13 @@ public class Enemy : MonoBehaviour
      // script to instantiate enemy vfx and parent it under empty gameobject.
 
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+
+    [SerializeField] int maxHealth = 6; // The maximum health of the enemy
+    int currentHealth; // The current health of the enemy
+
 
     ScoreBoard scoreBoard;
 
@@ -16,18 +21,26 @@ public class Enemy : MonoBehaviour
     void Start()
         // Find first object in project named scoreboard.
     {
+        currentHealth = maxHealth; // set the current health to the maximum health at the start
         scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
      void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        
 
     }
 
     void ProcessHit()
     {
+        currentHealth -= 1; // reduce the current health by 1 when hit by the player
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        if (currentHealth <= 0)
+        {
+            KillEnemy(); // if the current health is less than or equal to 0, call the KillEnemy method
+        }
         scoreBoard.IncreaseScore(scorePerHit);
     }
 
